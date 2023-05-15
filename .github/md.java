f2020afee730b1e4b3d7ed81b7f3b4ff42fddfaf
PareshNair <71382988+PN2421@users.noreply.github.com>
2023-05-15 11:58:26 +0530

    md.java

--- .github/md.java
+++ .github/md.java
@@ -1,50 +1,107 @@
+// server code:
+
+import java.io.DataOutputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
-public class md {
-
-public class FileMessageDigest {
+public class MessageDigestServer {
     public static void main(String[] args) {
-        String fileName = "example.txt";
-        
         try {
-            MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
-            FileInputStream fileInputStream = new FileInputStream(fileName);
-            byte[] buffer = new byte[1024];
-            int bytesRead;
-            
-            while ((bytesRead = fileInputStream.read(buffer)) != -1) {
-                messageDigest.update(buffer, 0, bytesRead);
+            ServerSocket serverSocket = new ServerSocket(1234);
+
+            System.out.println("Server started. Waiting for client...");
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Client connected. Starting message digest computation...");
+
+                Thread clientThread = new ClientThread(clientSocket);
+                clientThread.start();
             }
-            
-            fileInputStream.close();
-            byte[] digest = messageDigest.digest();
-            
-            System.out.println("Message Digest (SHA-256) for " + fileName + ":");
-            System.out.println(bytesToHex(digest));
-            
-        } catch (NoSuchAlgorithmException | IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    
-    private static String bytesToHex(byte[] bytes) {
-        StringBuilder hexString = new StringBuilder(2 * bytes.length);
-        
-        for (byte b : bytes) {
-            String hex = Integer.toHexString(0xff & b);
-            
-            if (hex.length() == 1) {
-                hexString.append('0');
-            }
-            
-            hexString.append(hex);
+
+    static class ClientThread extends Thread {
+        private Socket clientSocket;
+
+        public ClientThread(Socket clientSocket) {
+            this.clientSocket = clientSocket;
         }
-        
-        return hexString.toString();
+
+        public void run() {
+            try {
+                DataOutputStream outputStream = new DataOutputStream(clientSocket.getOutputStream());
+
+                String filePath = "path/to/your/file"; // Specify the path to your file
+
+                String messageDigest = computeMessageDigest(filePath);
+
+                outputStream.writeBytes(messageDigest + '\n');
+                System.out.println("Message digest sent to client: " + messageDigest);
+
+                clientSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private String computeMessageDigest(String filePath) {
+            try {
+                MessageDigest md = MessageDigest.getInstance("SHA-256");
+                FileInputStream fis = new FileInputStream(filePath);
+                byte[] buffer = new byte[8192];
+                int bytesRead;
+                while ((bytesRead = fis.read(buffer)) != -1) {
+                    md.update(buffer, 0, bytesRead);
+                }
+                fis.close();
+
+                byte[] digest = md.digest();
+                StringBuilder sb = new StringBuilder();
+                for (byte b : digest) {
+                    sb.append(String.format("%02x", b));
+                }
+                return sb.toString();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 }
 
+//client code:
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class MessageDigestClient {
+    public static void main(String[] args) {
+        try {
+            Socket clientSocket = new Socket("localhost", 1234);
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            DataOutputStream outputStream = new DataOutputStream(clientSocket.getOutputStream());
+
+            System.out.print("Enter the path to the file: ");
+            String filePath = reader.readLine();
+
+            outputStream.writeBytes(filePath + '\n');
+
+            String messageDigest = reader.readLine();
+            System.out.println("Message Digest: " + messageDigest);
+
+            clientSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
